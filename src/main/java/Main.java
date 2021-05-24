097ae946db755a6172de089c20bb30a1abb2e2d8
aaricrubel <54011160+aaricrubel@users.noreply.github.com>
2021-05-25 00:16:15 +0530

    Added session storing function to prevent logging in multiple times.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import java.util.NoSuchElementException;
